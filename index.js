f267325e1e534c3577dc2f1368a5c0400554baf9
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2019-01-30 23:07:30 -0800

    finished with app

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 // Create a component
-const App = () => {
-  return (
-    <View>
-      <Header headerText={'Albums'} />
-      <AlbumList />
-    </View>
-  );
-};
+const App = () => (
+        <View style={{ flex: 1 }}>
+            <Header headerText={'Albums'} />
+            <AlbumList />
+        </View>
+    );
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,29 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+  state = {
+    albums: []
+  };
+
   componentWillMount() {
-    console.log('componentWillMount in AlbumList');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(res => this.setState({
+        albums: res.data
+      }));
   }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
+  }
+
   render() {
     return (
-      <View>
-        <Text>Album List!!!!</Text>
-      </View>
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
     );
   }
 }
